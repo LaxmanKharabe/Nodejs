8805c31d862240e5579fec1b66944487765e3b19
LaxmanKharabe <laxman31097@gmail.com>
2026-01-09 20:15:23 +0530

    practiced and added the notes files

--- NamasteDev/exports_require/output.js
+++ NamasteDev/exports_require/output.js
@@ -1,6 +1,11 @@
 /*Example of nested exports*/
 
 const obj = require("./mainModule");  // we have directly used folder name instead of file name index.js
+const o = require("./practice.js");
+
+const constVariable = 20;
+let variableLet = 10;
+console.log(constVariable, "constVariable", variableLet, o.constVariable, o.variableLet);
 
 obj.sum.sum(5, 3);  
 obj.sub.sub(10, 4);
---
+++ NamasteDev/exports_require/practice.js
@@ -0,0 +1,5 @@
+const constVariable = "This is a constant variable";
+let variableLet = "This is a variableLet variable";
+var variableVar = "This is a variableVar variable";
+
+module.exports = { constVariable, variableLet, variableVar };
